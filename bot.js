3680ed3ed086c4b68d65f8e7234c245588794e3b
Joine0 <38402849+Joine0@users.noreply.github.com>
2018-04-15 19:14:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
-const Discord = require('discord.js');//lllll
-const client = new Discord.Client();//fffff
+const Discord = require('discord.js');
+const client = new Discord.Client();
 client.on('ready', () => {
 console.log('Logging into discord..');
 console.log(`
